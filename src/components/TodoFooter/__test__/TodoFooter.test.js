81eb928fa83040d13e82675d4dd0a99c5ec8a66b
Samvardhan163 <samvardhan.gopathy@thoughtworks.com>
2022-06-22 10:21:19 +0530

    refactored the code

--- src/components/TodoFooter/__test__/TodoFooter.test.js
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import TodoFooter from "../TodoFooter";
 
 describe("TodoFooter", () => {
-  it("should render the same text passed into the props", () => {
+  it("should render the plus icon", () => {
     const { container } = render(<TodoFooter />);
 
     expect(container.getElementsByClassName("plus").length).toBe(1);
